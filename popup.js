ef3557d810c0c1d743a60976e8fa612bbac35fa4
Jason <guojingwei129@gmail.com>
2024-08-28 09:41:59 +0800

    增加断开重连功能

--- popup.js
+++ popup.js
@@ -6,17 +6,19 @@ document.getElementById('save').addEventListener('click', () => {
   chrome.runtime.sendMessage({ action: "updateProxy", host, port }, (response) => {
     if (chrome.runtime.lastError) {
       setModelMsg(chrome.runtime.lastError.message)
+      onStartup()
     } else {
       if (response.success) {
         setModelMsg("GProxy Connect Success !")
         const resetButton = document.getElementById('reset');
         resetButton.hidden = false; // 显示按钮
         const saveButton = document.getElementById('save');
-        saveButton.hidden = true; // 隐藏按钮
+        saveButton.hidden = true;
         isconnected = true;
         chrome.storage.local.set({ isconnected: true });
       } else {
         setModelMsg("Failed to connect gproxy settings.")
+        onStartup()
       }
     }
   });
@@ -29,31 +31,28 @@ document.getElementById('reset').addEventListener('click', () => {
   chrome.runtime.sendMessage({ action: "disconnectProxy", host, port }, (response) => {
     if (chrome.runtime.lastError) {
       setModelMsg(chrome.runtime.lastError.message)
+      onStartup()
     } else {
       if (response.success) {
         setModelMsg("GProxy Disconnect Success !")
         const resetButton = document.getElementById('reset');
-        resetButton.hidden = true; // 显示按钮
+        resetButton.hidden = true;
         const saveButton = document.getElementById('save');
-        saveButton.hidden = false; // 隐藏按钮
+        saveButton.hidden = false;
         isconnected = false;
         chrome.storage.local.set({ isconnected: false });
       } else {
         setModelMsg("Failed to disconnect gproxy.")
+        onStartup()
       }
     }
   });
 });
 
 function setModelMsg(msg){
-  // 设置模态框内容
   document.getElementById('modal-text').textContent = msg;
-
-  // 显示模态框
   var modal = document.getElementById('modal');
   modal.classList.add('show');
-
-  // 模态框2秒后逐渐消失
   setTimeout(function() {
     modal.style.transition = 'opacity 1s ease-out';
     modal.style.opacity = '0';
@@ -64,18 +63,17 @@ function setModelMsg(msg){
   }, 1500);
 }
 function onStartup() {
-  // 读取布尔值
   chrome.storage.local.get('isconnected', (result) => {
     if (result.isconnected === true) {
       const resetButton = document.getElementById('reset');
-      resetButton.hidden = false; // 显示按钮
+      resetButton.hidden = false;
       const saveButton = document.getElementById('save');
-      saveButton.hidden = true; // 隐藏按钮
+      saveButton.hidden = true;
     } else {
       const resetButton = document.getElementById('reset');
-      resetButton.hidden = true; // 隐藏按钮
+      resetButton.hidden = true;
       const saveButton = document.getElementById('save');
-      saveButton.hidden = false; // 显示按钮
+      saveButton.hidden = false;
     }
   });
 }
