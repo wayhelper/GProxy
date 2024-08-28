89955b9b307b480e2b34978bcb05592978b3db1a
Jason <guojingwei129@gmail.com>
2024-08-28 08:55:45 +0800

    增加断开重连功能

--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
+var isconnected = false;
 document.getElementById('save').addEventListener('click', () => {
   const host = document.getElementById('host').value;
   const port = document.getElementById('port').value;
@@ -8,8 +9,37 @@ document.getElementById('save').addEventListener('click', () => {
     } else {
       if (response.success) {
         setModelMsg("GProxy Connect Success !")
+        const resetButton = document.getElementById('reset');
+        resetButton.hidden = false; // 显示按钮
+        const saveButton = document.getElementById('save');
+        saveButton.hidden = true; // 隐藏按钮
+        isconnected = true;
+        chrome.storage.local.set({ isconnected: true });
       } else {
         setModelMsg("Failed to connect gproxy settings.")
+      }
+    }
+  });
+});
+
+document.getElementById('reset').addEventListener('click', () => {
+  const host = document.getElementById('host').value;
+  const port = document.getElementById('port').value;
+
+  chrome.runtime.sendMessage({ action: "disconnectProxy", host, port }, (response) => {
+    if (chrome.runtime.lastError) {
+      setModelMsg(chrome.runtime.lastError.message)
+    } else {
+      if (response.success) {
+        setModelMsg("GProxy Disconnect Success !")
+        const resetButton = document.getElementById('reset');
+        resetButton.hidden = true; // 显示按钮
+        const saveButton = document.getElementById('save');
+        saveButton.hidden = false; // 隐藏按钮
+        isconnected = false;
+        chrome.storage.local.set({ isconnected: false });
+      } else {
+        setModelMsg("Failed to disconnect gproxy.")
       }
     }
   });
@@ -33,3 +63,20 @@ function setModelMsg(msg){
     }, 1000);
   }, 1500);
 }
+function onStartup() {
+  // 读取布尔值
+  chrome.storage.local.get('isconnected', (result) => {
+    if (result.isconnected === true) {
+      const resetButton = document.getElementById('reset');
+      resetButton.hidden = false; // 显示按钮
+      const saveButton = document.getElementById('save');
+      saveButton.hidden = true; // 隐藏按钮
+    } else {
+      const resetButton = document.getElementById('reset');
+      resetButton.hidden = true; // 隐藏按钮
+      const saveButton = document.getElementById('save');
+      saveButton.hidden = false; // 显示按钮
+    }
+  });
+}
+onStartup()
